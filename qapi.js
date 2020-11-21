9d44c40d3d0ca395c622895f712f07c319647c47
aaufec <74836481+aaufec@users.noreply.github.com>
2020-11-21 20:58:17 +0100

    Initial commit

---
+++ qapi.js
@@ -0,0 +1,25 @@
+export default {
+	"pages": {
+		"root": {
+			"id": "root",
+			"pageUrl": "root",
+			"name": "root",
+			"children": [
+				"5fb9705079530b001fd2b254",
+				"5fb9705079530b001fd2b256"
+			]
+		},
+		"5fb9705079530b001fd2b254": {
+			"id": "5fb9705079530b001fd2b254",
+			"name": "404",
+			"pageUrl": "404"
+		},
+		"5fb9705079530b001fd2b256": {
+			"id": "5fb9705079530b001fd2b256",
+			"name": "index",
+			"pageUrl": "index"
+		}
+	},
+	"mode": "production",
+	"projectType": "gatsby"
+}
